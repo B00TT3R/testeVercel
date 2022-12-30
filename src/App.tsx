070a294a7cb342887727d19c53dfab2c13ce67b0
B00TT3R <boott3r@gmail.com>
2022-12-30 10:45:32 -0400

    commit teste vercel

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <div className="App">
@@ -15,17 +15,15 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Teste do Vercel</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Frango frito com farofa de banana
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Eu estou morrendo de frio.
       </p>
     </div>
   )
